e02c67834ddcf568b81bbfac8399c0f27c24af7b
ishudhariwal <91891379+ishudhariwal@users.noreply.github.com>
2022-03-06 16:08:24 +0530

    update

--- src/Components/AddMentor/AddMentor.jsx
+++ src/Components/AddMentor/AddMentor.jsx
@@ -1,11 +1,34 @@
 import "./AddMentor.scss";
 
 import ClearIcon from "@mui/icons-material/Clear";
-import React from "react";
+import React, {useState} from "react";
 
 const AddMentor = ({setaddmentorOpen, addmentorOpen}) => {
+  const [mentor, setmentor] = useState({
+    MentorID: 0,
+    MentorNAME: "",
+    CreatedOn: "",
+    MentorEMAIL: "",
+    MentorPH: "",
+    MentorLINKEDIN: "",
+    MentorINSTA: "",
+    MentorPM: "",
+  });
+
   const handleCross = () => {
     setaddmentorOpen(!addmentorOpen);
+  };
+
+  const onChange = (e) => {
+    const re = /^[0-9\b]+$/;
+
+    // if value is not blank, then test the regex
+
+    if (e.target.value === "" || re.test(e.target.value)) {
+      this.setState({value: e.target.value});
+    }
+    setmentor({...mentor, [e.target.name]: e.target.value});
+    console.log(e.target.value);
   };
   const onSubmit = (e) => {
     e.preventDefault();
@@ -18,12 +41,12 @@ const AddMentor = ({setaddmentorOpen, addmentorOpen}) => {
         <h1>Add a mentor</h1>
         <ClearIcon className="icon" onClick={handleCross} />
       </div>
-      <input type="number" name="MentorID" placeholder="Mentor ID"></input>
-      <input name="MentorNAME" placeholder="Full Name"></input>
-      <input name="MentorEMAIL" placeholder="Email"></input>
-      <input name="MentorPH" placeholder="Phone No."></input>
-      <input name="MentorLINKEDIN" placeholder="Linkedin ID"></input>
-      <input name="MentorPM" placeholder="Placements"></input>
+      <input type="text" name="MentorID" placeholder="Mentor ID" onChange={onChange} />
+      <input name="MentorNAME" placeholder="Full Name" onChange={onChange} />
+      <input name="MentorEMAIL" placeholder="Email" onChange={onChange} />
+      <input name="MentorPH" placeholder="Phone No." onChange={onChange} />
+      <input name="MentorLINKEDIN" placeholder="Linkedin ID" onChange={onChange} />
+      <input name="MentorPM" placeholder="Placements" onChange={onChange} />
       <button type="submit">Add Mentor</button>
     </form>
   );
